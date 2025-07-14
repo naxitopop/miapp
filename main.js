0435ef8aacdd9f1581d39b65d400de34656a2f29
mafinde <mafindiepumapam@gmail.com>
2025-07-14 15:08:20 -0400

    add sequencial id

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const formulario = document.querySelector('.form-create');
+let contador = 1;
+
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const publiacion = document.querySelector('#publicacion').value;
     const contenedor = document.querySelector('.contenedor-publicaciones');
+    
     contenedor.innerHTML += `
     <div class="contenedor-publicaciones">
-        <p>Id: 00</p>
+        <p>Id: ${contador}</p>
         <p>${publiacion}</p>
         <button>Editar</button>
         <button>Eliminar</button>
     </div>`
+    contador++;
 });
